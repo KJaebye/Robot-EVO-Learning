1d3c2faa885717f36e7890ee4a48c7d9dae62258
Derkai52 <2363104914@qq.com>
2023-04-22 02:29:08 +0800

    Init ant-dev branch, and add ant model

--- main.py
+++ main.py
@@ -18,7 +18,12 @@ def main():
     env = make_env(args.env, render_mode="human")
     env.seed(seed=42)
 
-    num_param = 8 #8
+    if args.env.startswith("AugmentBipedalWalker"):
+        num_param = 8
+    elif args.env.startswith("AugmentAnt"):
+        num_param = 36
+    else:
+        print("This agent is undefined")
     augment_vector = (1.0 + (np.random.rand(num_param)*2-1.0)*0.5)
     env.augment_env(augment_vector)
     observation, info = env.reset()
